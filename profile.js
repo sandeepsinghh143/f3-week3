8b44aefc5bfbbe6ed856d86dbbf6bbf39c906739
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2023-07-02 00:09:24 +0530

    changes

--- profile.js
+++ profile.js
@@ -12,10 +12,35 @@ saveInfoBtn.addEventListener("click",function(){
     currentUser.fName = fName;
     currentUser.lName = lName;
     localStorage.setItem("currentUser", JSON.stringify(currentUser));
-    users[currentUser.id-1]=currentUser;
+    for(let i=0;i<users.length;i++){
+        if(users[i].email === currentUser.email){
+            users[i]=currentUser;
+        }
+    }
     localStorage.setItem("usersData", JSON.stringify(users));
 });
+
 let changePasswordBtn=document.getElementById('changePasswordBtn');
+changePasswordBtn.addEventListener("click",function(){
+    let users=JSON.parse(localStorage.getItem("usersData"));
+    let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    let oldPassword=document.getElementById("oldPass").value.trim();
+    let newPassword=document.getElementById("newPass").value.trim();
+    let confNewPassword=document.getElementById("confNewPass").value.trim();
+    if(oldPassword==currentUser.password){
+        if(newPassword==confNewPassword){
+            currentUser.password=newPassword;
+        }
+    }
+    localStorage.setItem("currentUser",JSON.stringify(currentUser));
+    for(let i=0;i<users.length;i++){
+        if(users[i].email === currentUser.email){
+            users[i]=currentUser;
+        }
+    }
+    localStorage.setItem("usersData",JSON.stringify(users));
+});
+
 let logOutBtn=document.getElementById('logOutBtn');
 logOutBtn.addEventListener('click',function(){
     localStorage.removeItem("currentUser");
